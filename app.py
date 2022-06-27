d5511d2681e8da0e6f5fabc0050e0f1fe0383495
Chavda Deep <82630272+Deepchavda007@users.noreply.github.com>
2022-06-27 11:21:33 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import pickle
+import numpy as np
+import streamlit as st
+
+
+profit_model = pickle.load(open(
+    'profit_model.sav', 'rb'))
+
+st.title('Companies Profit Predictions')
+
+
+rdspend = st.number_input('Research and Development (R&D) Spend',
+                          min_value=1000., step=1000.)
+st.write('The current Value is ', rdspend)
+
+administration = st.number_input('Administration', min_value=1000., step=1000.)
+st.write('The current Value is ', administration)
+
+
+marketingspend = st.number_input(
+    'Marketing Spend', min_value=1000., step=1000.)
+st.write('The current Value is ', marketingspend)
+
+
+st.button('Companies Profit Result')
+
+profit_prediction = profit_model.predict(
+    [[rdspend, administration, marketingspend]])
+
+st.success(profit_prediction)
